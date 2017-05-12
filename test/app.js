39f509299845bb405f8b825cb2d4384486e32f43
SHUBHAM DEB <debshubham@gmail.com>
2017-05-12 15:48:59 -0400

    Added some changes to the /test/app.js file

--- test/app.js
+++ test/app.js
@@ -4,14 +4,12 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
-
-    if(process.env.MLAB_USERNAME_WEBDEV) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD_WEBDEV + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+        var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        connectionString = 'mongodb://' + username + ':' + password;
+        connectionString += 'mongodb://username:password@ds111791.mlab.com:11791/heroku_dpwjbc7x';
     }
 
     var mongoose = require("mongoose");
