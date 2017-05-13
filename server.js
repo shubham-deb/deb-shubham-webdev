1e1cb73e7536f90874bef83e903f5cedab2dfcf6
SHUBHAM DEB <debshubham@gmail.com>
2017-05-13 15:36:54 -0400

    made changes to the .gitignore and server.js file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
-require("./todo/app")(app);
+//require("./todo/app")(app);
 
 // receives either the port that the heroku uses or default port 3000
 var port = process.env.PORT || 3000;
