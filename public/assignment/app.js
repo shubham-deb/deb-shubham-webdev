f32d910c432e6be9423c08e9151a93b55c342e3d
SHUBHAM DEB <debshubham@gmail.com>
2017-06-12 23:09:01 -0400

    This is a another-pre-release

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -2,6 +2,14 @@
  * Created by debsh on 23-05-2017.
  */
 (function () {
+    var mongoose = require('mongoose');
+    mongoose.Promise = require('q').Promise;
+    var connectionString = 'mongodb://localhost/myDb';
+
+    if(process.env.MONGODB_URI){
+        connectionString = process.env.MONGODB_URI;
+    }
+
     if(process.env.MLAB_USERNAME) {
         connectionString = process.env.MLAB_USERNAME + ":" +
             process.env.MLAB_PASSWORD + "@" +
@@ -10,9 +18,8 @@
             process.env.MLAB_APP_NAME;
     }
 
-    var mongoose = require("mongoose");
     mongoose.connect(connectionString);
-    
+
     angular
         .module('WebAppMaker',['ngRoute','wbdvDirectives','textAngular']);
 })();
