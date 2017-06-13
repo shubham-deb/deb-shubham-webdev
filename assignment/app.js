d1deaea0b4bc1d6c5fad41911fec0e5533e0b4fa
SHUBHAM DEB <debshubham@gmail.com>
2017-06-12 23:19:36 -0400

    This is a another-pre-release

--- assignment/app.js
+++ assignment/app.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
+var connectionString = 'mongodb://localhost/myDb';
 
+if(process.env.MLAB_USERNAME_WEBDEV) {
+    var username = process.env.MLAB_USERNAME_WEBDEV;
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds111791.mlab.com:11791/heroku_dpwjbc7x';
+}
+
+mongoose.connect(connectionString);
 // mongoose.connect('mongodb://localhost/myDb');
 
 require('./services/user.service.server');
--- public/assignment/app.js
+++ public/assignment/app.js
@@ -2,19 +2,6 @@
  * Created by debsh on 23-05-2017.
  */
 (function () {
-    var mongoose = require('mongoose');
-    mongoose.Promise = require('q').Promise;
-    var connectionString = 'mongodb://localhost/myDb';
-
-    if(process.env.MLAB_USERNAME_WEBDEV) {
-        var username = process.env.MLAB_USERNAME_WEBDEV;
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
-        connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds111791.mlab.com:11791/heroku_dpwjbc7x';
-    }
-
-    mongoose.connect(connectionString);
-
     angular
         .module('WebAppMaker',['ngRoute','wbdvDirectives','textAngular']);
 })();
