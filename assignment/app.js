1b52b03aa081cbe461563820793aa7973e678d64
SHUBHAM DEB <debshubham@gmail.com>
2017-06-12 23:46:41 -0400

    This is a another-pre-release

--- assignment/app.js
+++ assignment/app.js
@@ -1,19 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
 
-// var connectionString = 'mongodb://localhost/myDb';
-
-if(process.env.MONGODB_URI){
-    connectionString = process.env.MONGODB_URI;
-}
-
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds111791.mlab.com:11791/heroku_dpwjbc7x'; // user yours
-}
-mongoose.connect(connectionString);
+// var connectionString = 'mongodb://localhost/myDb';\
+connectionString = 'mongodb://shubhamdeb:shubhamdeb@123@ds111791.mlab.com:11791/heroku_dpwjbc7x'; // user yours
 // mongoose.connect('mongodb://localhost/myDb');
 
 require('./services/user.service.server');
