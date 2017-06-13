461e2df0c38f067cc218e987ab6469299963a1a5
SHUBHAM DEB <debshubham@gmail.com>
2017-06-12 22:37:47 -0400

    This is a another-pre-release

--- assignment/app.js
+++ assignment/app.js
@@ -3,7 +3,11 @@ mongoose.Promise = require('q').Promise;
 
 // mongoose.connect('mongodb://localhost/myDb');
 
-// var connectionString = 'mongodb://localhost/myDb';
+var connectionString = 'mongodb://localhost/myDb';
+
+if(process.env.MONGODB_URI){
+    connectionString = process.env.MONGODB_URI;
+}
 
 if(process.env.MLAB_USERNAME) {
     connectionString = process.env.MLAB_USERNAME + ":" +
@@ -12,7 +16,6 @@ if(process.env.MLAB_USERNAME) {
         process.env.MLAB_PORT + '/' +
         process.env.MLAB_APP_NAME;
 }
-
 mongoose.connect(connectionString);
 
 require('./services/user.service.server');
